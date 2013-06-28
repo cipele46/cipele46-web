296b71d0e400ebe01d1da3383a94c24aba84ae03
Sasa Brankovic <sasa@hakeraj.com>
2013-06-28 16:02:35 +0200

    Mail fix.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,21 +17,19 @@ Cipele46Web::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: " smtp.live.com",
     port: 587,
-    domain: "example.com",
+    domain: "cipele46.org",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"]
   }
-
-
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
