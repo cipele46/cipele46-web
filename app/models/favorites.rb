896fd2a263b69ee53f2ff8188f64d4c9ed2fc571
Sasa Brankovic <sasa@hakeraj.com>
2013-07-05 19:45:57 +0200

    Adding models.

---
+++ app/models/favorites.rb
@@ -0,0 +1,5 @@
+class Favorites < ActiveRecord::Base
+  belongs_to :ad
+  belongs_to :user
+  # attr_accessible :title, :body
+end
---
+++ spec/models/favorites_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Favorites do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
