0367ec6abe4b2e133437a6a38123637d1ec37451
Sasa Brankovic <sasa@hakeraj.com>
2013-07-05 19:55:26 +0200

    Ads admin.

--- db/schema.rb
+++ db/schema.rb
@@ -13,23 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130705174133) do
 
-  create_table "ads", :force => true do |t|
-    t.string   "title"
-    t.string   "image_url"
-    t.text     "description"
-    t.integer  "type"
-    t.integer  "status"
-    t.integer  "category_id"
-    t.integer  "user_id"
-    t.integer  "city_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  add_index "ads", ["category_id"], :name => "index_ads_on_category_id"
-  add_index "ads", ["city_id"], :name => "index_ads_on_city_id"
-  add_index "ads", ["user_id"], :name => "index_ads_on_user_id"
-
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
     t.string   "resource_type", :null => false
@@ -62,6 +45,23 @@ ActiveRecord::Schema.define(:version => 20130705174133) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "ads", :force => true do |t|
+    t.string   "title"
+    t.string   "image_url"
+    t.text     "description"
+    t.integer  "type"
+    t.integer  "status"
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.integer  "city_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "ads", ["category_id"], :name => "index_ads_on_category_id"
+  add_index "ads", ["city_id"], :name => "index_ads_on_city_id"
+  add_index "ads", ["user_id"], :name => "index_ads_on_user_id"
 
   create_table "categories", :force => true do |t|
     t.string   "name"
