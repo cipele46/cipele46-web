1f23bd188dbb3cdb39c810176f14794ab64f4747
sloser <sloser@emusoft.hr>
2013-07-05 19:34:38 +0200

    admin initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Cipele46Web::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
   devise_for :users
+  ActiveAdmin.routes(self)
   resources :users
 end
