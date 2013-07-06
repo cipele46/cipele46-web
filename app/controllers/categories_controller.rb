7e0a3a010b07aafb1d70b8e1ad13a7b668c0e49f
Vlado Cingel <vladocingel@gmail.com>
2013-07-06 15:06:50 +0200

    Categories cleanup

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,9 @@
 class CategoriesController < ApplicationController
+  respond_to :json
+
   def index
-    @categories = Category.find(:all, :order => :name)
-     
-    respond_to do |format|
-      format.json do 
-        render :json => @categories
-      end
-    end
+    @categories = Category.order(:name)
+    respond_with @categories
   end
-  
+
 end
