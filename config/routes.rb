2d5e21344277fa96fcd64fa1a849752b7c798c7c
Tomislav Capan <tomislav.capan@gmail.com>
2013-07-06 13:14:03 +0200

    fixing active admin root route overriding public app root route see http://stackoverflow.com/questions/15818446/

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Cipele46Web::Application.routes.draw do
   get "categories/index"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  root :to => "home#index"
+
   ActiveAdmin.routes(self)
 
-  
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
   get 'home' => 'home#index'
   devise_for :users
-  
+
   resources :ads, :only => [:create, :edit, :new, :show, :update]
   resources :users
   resources :regions, :only => [:index]
