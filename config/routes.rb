66da90756fd0b2086a6cc74e33adfc1f0d600e68
sloser <sloser@emusoft.hr>
2013-07-06 12:53:34 +0200

    image variations

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Cipele46Web::Application.routes.draw do
   get "categories/index"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
+  
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
   get 'home' => 'home#index'
   devise_for :users
-  ActiveAdmin.routes(self)
+  
   resources :ads, :only => [:create, :edit, :new, :show, :update]
   resources :users
   resources :regions, :only => [:index]
