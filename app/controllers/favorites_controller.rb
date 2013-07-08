d8ae1145b4c4fafbfe6a0fff7151968097edb005
Sasa Brankovic <sasa@hakeraj.com>
2013-07-09 00:09:25 +0200

    Favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,14 @@
 class FavoritesController < ApplicationController
   before_filter :authenticate_user!
-  respond_to :html, :json
+  respond_to :json
 
   def toggle
     ad_id = params[:id].to_i
+
+    unless Ad.exists?(ad_id)
+      respond_with "No such ad #{ad_id}", :status => 404
+      return
+    end
 
     favorite = Favorite.where("user_id = ? AND ad_id = ?", current_user.id, ad_id)
 
@@ -15,5 +20,7 @@ class FavoritesController < ApplicationController
       favorite.ad_id = ad_id
       favorite.save
     end
+    
+    respond_with favorite
   end
 end
