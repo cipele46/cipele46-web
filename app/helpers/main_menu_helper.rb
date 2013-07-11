37e6990c1b05abe5d748f733cf1f6cb20a3d62b8
Vlado Cingel <vladocingel@gmail.com>
2013-07-11 17:31:01 +0200

    main menu navigation with "current" item logic

---
+++ app/helpers/main_menu_helper.rb
@@ -0,0 +1,26 @@
+# encoding: UTF-8
+
+module MainMenuHelper
+
+  def main_menu
+    current_path = request.fullpath
+    items = main_menu_items.map do |item|
+      link = link_to item[0], item[1]
+      klass = current_path.match(item[2]) ? 'current' : nil
+      content_tag(:li, link, :class => klass)
+    end
+    content_tag :ul, sanitize(items.join)
+  end
+
+
+  private
+
+  def main_menu_items
+    [
+      ["oglasi", root_path, /\A(\/\Z|\/ads)/],
+      # ["blog", blogs_path, //],
+      ["naša priča", page_path('nasa-prica'), /\A\/pages\/nasa-prica\Z/]
+    ]
+  end
+
+end
