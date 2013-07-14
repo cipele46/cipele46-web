19a271459e2373dc7e3410dbbbc260344d5cbea6
antebarac <ante.barac@fiveminutes.eu>
2013-07-14 22:51:21 +0200

    missing tab

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :rails_env,   'staging'
 set :deploy_to,   "/home/#{user}/#{application}"
 set :branch,      'dev'
 set :solr_port,   8984
-set :solr_data,  'data-staging'
+set :solr_data,   'data-staging'
 
 role :web, 'staging.cipele46.org'                         
 role :app, 'staging.cipele46.org'                       
