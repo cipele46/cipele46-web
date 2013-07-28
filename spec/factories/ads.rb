03ae0e7712ea34405052c4d704af5441c6763519
Hrvoje Šimić <shime.ferovac@gmail.com>
2013-07-28 13:43:10 +0200

    add factories for blogs

--- spec/factories/ads.rb
+++ spec/factories/ads.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-DESCRIPTION = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt"\
+AD_DESCRIPTION ||= "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt"\
       " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"\
       " laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in"\
       " voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat"\
       " non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-TITLES = ["Poklanjam kaput", "Trebalo bi mi 100 kuna", "Tražim plave papuče", "Poklanjam papigu",
+AD_TITLES ||= ["Poklanjam kaput", "Trebalo bi mi 100 kuna", "Tražim plave papuče", "Poklanjam papigu",
    "Trebalo bi mi 200 kuna", "Tražim cipele 47", "Poklanjam commodore 64", "Trebalo bi mi 300 kuna",
    "Tražim zelene papuče", "Poklanjam ženu", "Trebalo bi mi 400 kuna", "Tražim aparat za gašenje požara",
    "Poklanjam babu", "Trebalo bi mi 500 kuna", "Tražim kunu za pivo", "Poklanjam punicu", "Trebalo bi mi 600 kuna",
@@ -16,10 +16,10 @@ TITLES = ["Poklanjam kaput", "Trebalo bi mi 100 kuna", "Tražim plave papuče", 
 
 FactoryGirl.define do
   factory :ad do
-    title TITLES.sample
+    title { AD_TITLES.sample }
     image { File.open("db/seed/adSeedImg#{(1..3).to_a.sample}.jpg") }
-    description DESCRIPTION
-    ad_type Ad.type.values.sample
+    description AD_DESCRIPTION
+    ad_type { Ad.type.values.sample }
     status Ad.status[:active]
     phone "+38591111111"
     email { "email#{(1..10).map { rand(10) }.join}@domena.hr" }
--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,9 +1,17 @@
+# encoding: utf-8
 # Read about factories at https://github.com/thoughtbot/factory_girl
+BLOG_DESCRIPTION ||= "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt"\
+      " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"\
+      " laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in"\
+      " <h1>voluptate</h1> <h2>velit</h2> <h3>esse</h3> cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat"\
+      " non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+BLOG_TITLES ||= ["Cipele našle svog vlasnika", "Caritas nepotreban uz nove super stranice", "Nestašica kratkih majica u Đakovu"]
 
 FactoryGirl.define do
   factory :blog do
-    title "MyString"
-    content "MyText"
-    image "MyString"
+    title { BLOG_TITLES.sample }
+    content BLOG_DESCRIPTION
+    image { File.open("db/seed/adSeedImg#{(1..3).to_a.sample}.jpg") }
   end
 end
