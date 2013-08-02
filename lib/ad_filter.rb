a70d2cf7c4b3442adb94279a4a65d56e00cd46b5
Hrvoje Šimić <shime.ferovac@gmail.com>
2013-08-02 09:22:00 +0200

    refactor AdFilter

--- lib/ad_filter.rb
+++ lib/ad_filter.rb
@@ -1,17 +1,11 @@
 class AdFilter
   FILTER_ATTRIBUTES = [:region_id, :category_id, :ad_type]
 
-  attr_accessor :region_id, :category_id, :ad_type, :query, :page, :per_page
+  attr_accessor :region_id, :category_id, :ad_type, :query, :page, :per_page, :email
 
   def initialize(params = {})
-    # TODO: refactor me
-    params ||= {}
-    params.each do |key, value|
-      if((value.instance_of?(Symbol) || value.instance_of?(String)) && self.respond_to?(key))        
-        value = value.present? && value.to_i != 0 ? value.to_i : nil unless key.to_sym == :query
-        value = nil if value.blank?
-        instance_variable_set("@#{key}", value) unless value.nil?
-      end
+    normalized_hash_of(params).each do |key, value|
+      set_attribute key, to: value 
     end
   end
 
@@ -28,5 +22,68 @@ class AdFilter
       paginate(page: page || 1, per_page: per_page || Ad::PER_PAGE)
       order_by(:created_at, :desc) if query.blank?
     end
+  end
+
+  private
+
+    def normalized_hash_of(hash)
+      Helpers::NormalizedHash.new(hash).normalize
+    end
+
+    def set_attribute(name, opts = {})
+      send("#{name}=", opts[:to]) if self.respond_to?(name) 
+    end
+end
+
+module Helpers
+  class NormalizedValue
+    ALLOWED_TO_BE_STRING = [:query]
+
+    def initialize(original, key)
+      self.value = original
+      self.key = key.to_sym
+    end
+
+    def normalize
+      case 
+      when ALLOWED_TO_BE_STRING.include?(key)
+        value
+      when present?
+        value.to_i
+      end
+    end
+
+    private
+
+    def present?
+      !blank?
+    end
+
+    def blank?
+      value.blank? || value.to_i == 0
+    end
+
+    attr_accessor :value, :key
+  end
+
+  class NormalizedHash
+
+    def initialize(original_hash)
+      self.hash = original_hash
+    end
+
+    def normalize
+      result = {}
+
+      hash.each do |key, value|
+        result[key] = NormalizedValue.new(value, key).normalize
+      end
+
+      result
+    end
+
+    private
+
+    attr_accessor :hash
   end
 end
