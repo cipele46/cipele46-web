aea531715af057d86e04fb3f995345cf995423ce
Mijo Kaliger <mijo.kaliger@gmail.com>
2013-09-03 13:35:27 +0200

    - in attempt to fix staging/ads.json error 500, add RailsHelper to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   config.include(EmailSpec::Helpers)
   config.include(EmailSpec::Matchers)
+  config.include Sprockets::Helpers::RailsHelper, :type => :request
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
