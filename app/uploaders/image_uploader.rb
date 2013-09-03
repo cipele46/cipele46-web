3805e6b9f805779757eff0f25aabe566394efdf2
Mijo Kaliger <mijo.kaliger@gmail.com>
2013-09-03 14:24:31 +0200

    revert changes to spec_helper, ActionController::Base.helpers.asset_path is used in ImageUploader.default_url

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,14 +2,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  include Sprockets::Helpers::RailsHelper
-  include Sprockets::Helpers::IsolatedHelper
+  storage :file
 
   def default_url
-     asset_path("fallback/" + [version_name, "default.png"].compact.join('_')) 
+     ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_')) 
   end
-
-  storage :file
 
   def store_dir
     "system/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
