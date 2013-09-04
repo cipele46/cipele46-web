8638c936500edf44e8c3720f82a31eb8288aad61
Hrvoje Šimić <shime.ferovac@gmail.com>
2013-09-04 15:13:41 +0200

    sinatra + asset pipeline + carrierwave = <3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,21 +2,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  include Sprockets::Helpers::IsolatedHelper
-  include Sprockets::Helpers::RailsHelper
-
-  storage :file
-
-  def default_url
-     ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_')) 
-  end
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url
-    asset_path("images")
+    ActionController::Base.helpers.asset_path("images")
   end
 
   version :large do
