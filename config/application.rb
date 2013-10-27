b94061bfe52fee10b577c55f43014ea2487a714e
Vlado Cingel <vladocingel@gmail.com>
2013-10-27 14:57:10 +0100

    Favor hr translations

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Cipele46Web
       g.view_specs false
       g.helper_specs false
     end
-    
+
     config.to_prepare do
       DeviseController.respond_to :html, :json
     end
@@ -56,6 +56,7 @@ module Cipele46Web
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :hr
+    config.i18n.default_locale = :hr
     config.i18n.locale = :hr
 
     # Configure the default encoding used in templates for Ruby 1.9.
