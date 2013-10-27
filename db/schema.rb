6ff03206f928d37eeab63673a7e4a6ce8b02f36d
Hrvoje Šimić <shime.ferovac@gmail.com>
2013-10-27 15:02:22 +0100

    user authentication is not required for ad replying

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131027132853) do
+ActiveRecord::Schema.define(:version => 20131027140001) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -107,10 +107,10 @@ ActiveRecord::Schema.define(:version => 20131027132853) do
 
   create_table "replies", :force => true do |t|
     t.text     "content"
-    t.integer  "user_id"
     t.integer  "ad_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "email"
   end
 
   create_table "roles", :force => true do |t|
