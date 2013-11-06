75ed764c2997ea55e65697f8c1a63638ea0b271b
Ante Barac abarac@gmail.com <abarac@gmail.com>
2013-11-06 18:13:28 +0100

    setup capistrano for carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,15 +2,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  def store_dir
-    if [:environment, :development].include?(Rails.env.to_sym)
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    else
-      "/home/cipele46/cipele46#{ Rails.env == 'staging' ? 'staging' : '' }/shared/assets/images/uploads/" 
-      "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
-  end
-
   version :large do
     resize_to_fit(800, 600)
   end
